1c6122ad9b2b777226d930f21f5754067993b150
James Hart <hjhart@gmail.com>
2012-02-07 22:07:06 -0800

    Legit logging to syslog, and it actually works!

---
+++ README.md
@@ -0,0 +1,7 @@
+Had some problems with an error message here
+
+	ssh_askpass: exec(/usr/libexec/ssh-askpass): No such file or directory
+	
+The link below fixed my problem.
+
+http://blog.retep.org/2009/04/09/ssh-askpass-on-osx-10-5/
--- backup.rb
+++ backup.rb
@@ -1,11 +1,42 @@
-directories_to_backup = %w{~/Sites}# ~/Code ~/workspace ~/Documents ~/Desktop ~/Music ~/Pictures}
+require 'benchmark'
+require 'FileUtils'
+require 'Logger'
+require 'open3'
+
+directories_to_backup = %w{~/Sites ~/Code ~/workspace ~/Documents ~/Desktop ~/Music ~/Pictures}
 exclusions = %w{.DS_Store *.tar *.mp3 *.log}
-backup_location = "backup:/volume1/jhart-macbook-pro/"
+backup_user = 'root'
+backup_host = 'hjhart.dyndns.org'
+backup_root = '/volume1/jhart-macbook-pro/'
+backup_location = "#{backup_user}@#{backup_host}:#{backup_root}"
 
 rsync_options = "--verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete"
 exclusion_options = exclusions.map { |exclusion| "--exclude \"#{exclusion}\"" }.join(" ")
 
-directories_to_backup.each do |directory|
-  rsync_command = "rsync #{rsync_options} #{exclusion_options} #{directory} #{backup_location}"
-  output = system "#{rsync_command}"
+datetime = Time.now
+
+log_age = 'daily'
+log_file = "/var/log/rsync_backup.log"
+logger = Logger.new(log_file, log_age)
+
+logger.info("Started running at: #{Time.now}")
+
+run_time = Benchmark.realtime do
+  begin
+    directories_to_backup.each do |directory|
+      rsync_command = "rsync #{rsync_options} #{exclusion_options} #{directory} #{backup_location}"
+      logger.info("Running command below:")
+      logger.info(rsync_command)
+      Open3::popen3("#{rsync_command}") { |stdin, stdout, stderr|
+        tmp_stdout = stdout.read.strip
+        tmp_stderr = stderr.read.strip
+
+        logger.info "#{rsync_command}\n#{tmp_stdout}" unless tmp_stdout.empty?
+        logger.error "#{rsync_command}\n#{tmp_stderr}" unless tmp_stderr.empty?
+      }
+    end
+  rescue Errno::EACCES, Errno::ENOENT, Errno::ENOTEMPTY, Exception => e
+    logger.fatal(e.to_s)
+  end
 end
+logger.info "Finished running at: #{Time.now} - Execution time: #{run_time.to_s[0, 5]}"
