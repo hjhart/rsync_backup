a330454dd395a5c5afdde6810e7e60c01d95d1bb
James Hart <hjhart@gmail.com>
2012-02-12 10:20:25 -0800

    Added shell scripts to backup computers.

--- macbook-pro-backup.sh
+++ macbook-pro-backup.sh
@@ -1,12 +1,4 @@
-Had some problems with an error message here
-
-	ssh_askpass: exec(/usr/libexec/ssh-askpass): No such file or directory
-	
-The link below fixed my problem.
-
-http://blog.retep.org/2009/04/09/ssh-askpass-on-osx-10-5/
-
-# First time run will help:
+#!/bin/sh
 
 rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/Sites root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
 rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/Code root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
