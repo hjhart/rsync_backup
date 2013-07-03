4b30d651e594524b4160d413b1f5b3df8f05a30d
James Hart <hjhart@gmail.com>
2013-07-02 23:41:37 -0700

    macbook pro updated rsync

--- backup.rb
+++ backup.rb
@@ -3,11 +3,11 @@ require 'FileUtils'
 require 'Logger'
 require 'open3'
 
-directories_to_backup = %w{~/Sites ~/Code ~/workspace ~/Documents ~/Desktop ~/Music ~/Pictures}
+directories_to_backup = %w{~/Sites ~/Documents ~/Desktop ~/Lightroom ~/Music ~/Pictures}
 exclusions = %w{.DS_Store *.tar *.mp3 *.log}
 backup_user = 'root'
 backup_host = 'hjhart.dyndns.org'
-backup_root = '/volume1/jhart-macbook-pro/'
+backup_root = '/volume1/mac_mini/'
 backup_location = "#{backup_user}@#{backup_host}:#{backup_root}"
 
 rsync_options = "--verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete"
@@ -15,16 +15,20 @@ exclusion_options = exclusions.map { |exclusion| "--exclude \"#{exclusion}\"" }.
 
 datetime = Time.now
 
-log_age = 'daily'
-log_file = "/var/log/rsync_backup.log"
-logger = Logger.new(log_file, log_age)
-
-logger.info("Started running at: #{Time.now}")
+# log_age = 'daily'
+# log_file = "/var/log/rsync_backup.log"
+# logger = Logger.new(log_file, log_age)
+output_only = true
+# logger.info("Started running at: #{Time.now}")
 
 run_time = Benchmark.realtime do
   begin
     directories_to_backup.each do |directory|
       rsync_command = "rsync #{rsync_options} #{exclusion_options} #{directory} #{backup_location}"
+      if output_only
+        puts rsync_command; 
+        next
+      end
       logger.info("Running command below:")
       logger.info(rsync_command)
       Open3::popen3("#{rsync_command}") { |stdin, stdout, stderr|
--- macbook-pro-backup.sh
+++ macbook-pro-backup.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/sh -xe
 
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/Sites root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/Code root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/workspace root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/Documents root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/Desktop root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.mp3" --exclude "*.log" ~/Pictures root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Music root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Sites root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Code root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/workspace root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Documents root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Desktop root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Pictures root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Screencasts root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Design root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete-excluded --delete --exclude ".DS_Store" --exclude "*.log"                   ~/backup_rpx_laptop root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
+# this is turned off until music gets synced back onto another computer?
+# rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude ".DS_Store" --exclude "*.tar" --exclude "*.log" ~/Music root@hjhart.dyndns.org:/volume1/jhart-macbook-pro/
